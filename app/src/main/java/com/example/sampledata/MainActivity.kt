f4eb75fd8412f20995823d02df4c5387f1138ad3
arshnirnal <arshnirnal11@gmail.com>
2024-01-06 17:01:59 +0530

    app bar final

--- app/src/main/java/com/example/sampledata/MainActivity.kt
+++ app/src/main/java/com/example/sampledata/MainActivity.kt
@@ -17,25 +17,34 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsStartWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.progressSemantics
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarColors
 import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -46,11 +55,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sampledata.model.Item
 import com.example.sampledata.ui.theme.SampleDataTheme
 import com.example.sampledata.view.Items
+import com.example.sampledata.viewModel.ItemList
 import com.example.sampledata.viewModel.ItemViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -65,8 +78,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppScreen(itemList = itemViewModel.itemListResponse)
-                    itemViewModel.getItemsList()
+                   if (itemViewModel.itemListResponse.isEmpty()) {
+                       itemViewModel.getItemsList()
+                       Box(
+                           modifier = Modifier.fillMaxSize(),
+                           contentAlignment = Alignment.Center
+                       ) {
+                           CircularProgressIndicator(
+                               modifier = Modifier.size(50.dp, 50.dp)
+                           )
+                       }
+                   } else {
+                       AppScreen(itemList = itemViewModel.itemListResponse)
+                   }
                 }
             }
         }
@@ -86,65 +110,75 @@ fun AppScreen(itemList: List<Item>) {
 
     Scaffold(
         topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = Color.LightGray),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(90.dp),
-                title = {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 8.dp),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
+            Column {
+                TopAppBar(
+                    colors = TopAppBarDefaults.mediumTopAppBarColors(
+                        containerColor = Color(
+                            0xFFe7eaf7
+                        )
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(40.dp),
+                    title = {
                         Row(
                             modifier = Modifier
-                                .padding(start = 16.dp, end = 24.dp)
+                                .padding(start = 16.dp, end = 24.dp, top = 8.dp)
                                 .fillMaxWidth(),
-                            verticalAlignment = Alignment.CenterVertically,
+                            verticalAlignment = Alignment.Top,
                             horizontalArrangement = Arrangement.SpaceBetween
                         ) {
                             Text(
                                 text = "Explore",
-                                style = MaterialTheme.typography.titleLarge,
+                                style = TextStyle(
+                                    color = Color.Black,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 18.sp,
+                                )
                             )
                             Text(
                                 text = "Filter",
-                                style = MaterialTheme.typography.titleMedium,
-                                color = Color.Cyan
+                                style = TextStyle(
+                                    color = Color.Green,
+                                    fontSize = 14.sp,
+                                )
                             )
                         }
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(34.dp)
-                                .padding(end = 16.dp)
-//                                .background(Color.Blue,shape = MaterialTheme.shapes.small)
-                        ) {
-                            SearchBar(
-                                modifier = Modifier
-                                    .fillMaxWidth(),
-                                query = searchText,
-                                onQueryChange = { searchText = it },
-                                onSearch = { isSearchActive = true },
-                                active = isSearchActive,
-                                onActiveChange = { isSearchActive = it },
-                            ) {
-                                Text(
-                                    text = "Search",
-                                    style = MaterialTheme.typography.bodyMedium,
-                                    color = Color.Gray
-                                )
-                            }
-                        }
                     }
-                },
-            )
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp)
+                        .background(Color(0xFFe7eaf7))
+                ) {
+                    SearchBar(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 16.dp, end = 16.dp)
+                            .height(40.dp),
+                        query = searchText,
+                        onQueryChange = { searchText = it },
+                        onSearch = { isSearchActive = true },
+                        active = isSearchActive,
+                        onActiveChange = { isSearchActive = it },
+                        shape = RoundedCornerShape(20.dp),
+                        colors = SearchBarDefaults.colors(containerColor = Color(0xFFf7f6f7)),
+                        //                                colors = TextFieldDefaults.textFieldColors(containerColor = Color.Red),
+                    ) {
+                        Text(
+                            text = "Search",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.Gray
+                        )
+                    }
+                }
+            }
         },
         bottomBar = {
-            BottomNavigation{
+            BottomNavigation(
+                backgroundColor =  colors.background,
+            ){
                 screens.forEach { screen ->
                     BottomNavigationItem(
                         icon = { Icon(Icons.Rounded.AddCircle, contentDescription = null) },
@@ -163,28 +197,6 @@ fun AppScreen(itemList: List<Item>) {
             when (selectedScreen) {
                 "ListView" -> ItemList(itemList = itemList)
                 "GridView" -> ItemList(itemList = itemList)
-                "Dummy1" -> Box(){}
-                "Dummy2" -> Box(){}
-                "Dummy3" -> Box(){}
-            }
-        }
-    }
-}
-
-@Composable
-fun ItemList(itemList: List<Item>) {
-    LazyColumn{
-        items(itemList.size) { index ->
-            val item = itemList[index]
-            Items(item = item)
-
-            if (index < itemList.size - 1) {
-                Divider(
-                    modifier = Modifier
-                        .padding(start = 85.dp, end = 16.dp),
-                    color = Color.LightGray,
-                    thickness = 1.dp
-                )
             }
         }
     }
--- app/src/main/java/com/example/sampledata/viewModel/item_viewModel.kt
+++ app/src/main/java/com/example/sampledata/viewModel/item_viewModel.kt
@@ -1,34 +1,55 @@
 package com.example.sampledata.viewModel
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.sampledata.model.Item
 import com.example.sampledata.network.ApiService
+import com.example.sampledata.view.Items
 import kotlinx.coroutines.launch
 
+
+@Composable
+fun ItemList(itemList: List<Item>) {
+    LazyColumn{
+        items(itemList.size) { index ->
+            val item = itemList[index]
+            Items(item = item)
+
+            if (index < itemList.size - 1) {
+                Divider(
+                    modifier = Modifier
+                        .padding(start = 85.dp, end = 16.dp),
+                    color = Color.LightGray,
+                    thickness = 1.dp
+                )
+            }
+        }
+    }
+}
 class ItemViewModel : ViewModel() {
     var itemListResponse : List<Item> by mutableStateOf(listOf())
-//    var isLoading : Boolean by mutableStateOf(false)
-//    var isError : Boolean by mutableStateOf(false)
-private var errorMessage : String by mutableStateOf("")
+    private var errorMessage : String by mutableStateOf("")
 
     fun getItemsList() {
         viewModelScope.launch {
-//            isLoading = true
             val apiService = ApiService.getInstance()
             try {
                 val response = apiService.getItemsList()
                 itemListResponse = response.data.items
                 println(itemListResponse)
             } catch (e: Exception) {
-//                isError = true
-//                isLoading = false
                 errorMessage = e.message.toString()
             }
-//            isLoading = false
         }
     }
 }
